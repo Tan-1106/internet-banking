b8325633bbce00a456df2a07e0c00f07e9bbf28e
XungChan <nmdtruong18032004@gmail.com>
2025-05-19 16:28:52 +0700

    Update DepositPhoneMoney.kt

--- app/src/main/java/com/example/internetbanking/ui/customer/DepositPhoneMoney.kt
+++ app/src/main/java/com/example/internetbanking/ui/customer/DepositPhoneMoney.kt
@@ -1,25 +1,238 @@
 package com.example.internetbanking.ui.customer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Contacts
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.internetbanking.R
+import com.example.internetbanking.ui.shared.BalanceInformation
+import com.example.internetbanking.ui.theme.GradientColors
+import com.example.internetbanking.ui.theme.custom_light_green1
+import com.example.internetbanking.ui.theme.custom_light_green2
+import com.example.internetbanking.ui.theme.custom_mint_green
 import com.example.internetbanking.viewmodels.CustomerViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DepositPhoneMoneyScreen(
     customerViewModel: CustomerViewModel,
     navController: NavHostController
 ) {
+//    var scrollState = rememberScrollState()
+    var phoneNumber by remember { mutableStateOf("") }
+    val amounts = listOf(
+        "50.0000đ",
+        "100.0000đ",
+        "200.0000đ",
+        "500.0000đ",
+        "1.000.0000đ",
+        "2.000.0000đ",
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.sub_background1),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Scaffold(
+            containerColor = Color.Transparent,
+            topBar = {
 
+                TopAppBar(
+                    title = { Text("Deposit/Withdraw", color = custom_mint_green) },
+                    navigationIcon = {
+
+                        IconButton(onClick = { }) {
+                            Icon(
+                                Icons.Default.ArrowBack, contentDescription = null, tint =
+                                    custom_mint_green
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .background(
+                            brush = GradientColors.Green_DarkToLight
+                        ),
+                )
+            },
+            bottomBar = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .background(Color.White)
+                ) {
+
+                    ElevatedButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .padding(vertical = 5.dp, horizontal = 10.dp)
+                            .fillMaxSize(),
+                        shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = custom_light_green1
+                        )
+                    ) {
+                        Text("Continue", color = Color.White, fontWeight = FontWeight.Bold)
+                    }
+
+                }
+            },
+            modifier = Modifier
+                .systemBarsPadding()
+                .fillMaxSize()
+//                .verticalScroll(scrollState),
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .padding(10.dp)
+            ) {
+                BalanceInformation()
+                Spacer(Modifier.height(20.dp))
+                Text(
+                    text = "Transaction Information",
+                    color = custom_light_green2,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp
+                )
+                Spacer(Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Color.White,
+
+                            shape = RoundedCornerShape(corner = CornerSize(20)
+                            )
+                        ),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(modifier = Modifier.fillMaxWidth(0.8f)) {
+                        TextField(
+                            phoneNumber,
+                            onValueChange = { phoneNumber = it }, keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Number
+                            )
+
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            Icons.Default.Contacts, contentDescription = null,
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .size(30.dp)
+                        )
+                    }
+                }
+
+                Spacer(Modifier.height(30.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = Color.White,
+                            shape = RoundedCornerShape(
+                                corner = CornerSize(20.dp)
+                            )
+                        )
+                        .padding(10.dp)
+                ) {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(3),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 20.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+
+                        ) {
+
+                        items(items = amounts) { amount ->
+                            OutlinedButton(
+                                onClick = {},
+                                shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                                contentPadding = PaddingValues(all = 0.dp),
+                                modifier = Modifier.padding(5.dp)
+                            ) {
+                                Text(amount, fontSize = 15.sp, color = Color.Black)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
 
 @Preview(
-    showBackground = true
+    showBackground = true, showSystemUi = true
 )
 @Composable
-fun DepositPhoneMoneyScreenPreview(){
+fun DepositPhoneMoneyScreenPreview() {
     val fakeViewModel: CustomerViewModel = viewModel()
     val fakeNavController: NavHostController = rememberNavController()
 
